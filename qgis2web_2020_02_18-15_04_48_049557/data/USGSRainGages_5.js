53101d5e1f19247d3922ccc7566dc57b9ecedea0
ngiro1 <60405842+ngiro1@users.noreply.github.com>
2020-02-18 15:08:39 -0500

    Add files via upload

---
+++ qgis2web_2020_02_18-15_04_48_049557/data/USGSRainGages_5.js
@@ -0,0 +1,13 @@
+var json_USGSRainGages_5 = {
+"type": "FeatureCollection",
+"name": "USGSRainGages_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Precip_in": 1.97, "USGS_ID": "392703076162801" }, "geometry": { "type": "Point", "coordinates": [ -76.275107865767126, 39.450076321262827 ] } },
+{ "type": "Feature", "properties": { "Precip_in": 1.87, "USGS_ID": "01581753" }, "geometry": { "type": "Point", "coordinates": [ -76.344279785156402, 39.485945717076852 ] } },
+{ "type": "Feature", "properties": { "Precip_in": 1.68, "USGS_ID": "393126076244301" }, "geometry": { "type": "Point", "coordinates": [ -76.41276655717347, 39.52454697917819 ] } },
+{ "type": "Feature", "properties": { "Precip_in": 1.93, "USGS_ID": "393352076141501" }, "geometry": { "type": "Point", "coordinates": [ -76.242011052911934, 39.562113514946361 ] } },
+{ "type": "Feature", "properties": { "Precip_in": 1.83, "USGS_ID": "394237076210901" }, "geometry": { "type": "Point", "coordinates": [ -76.352781205610881, 39.710863945303949 ] } },
+{ "type": "Feature", "properties": { "Precip_in": 1.56, "USGS_ID": "394205076320201" }, "geometry": { "type": "Point", "coordinates": [ -76.539892855557255, 39.7111436140503 ] } }
+]
+}
